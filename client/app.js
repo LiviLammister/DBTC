6e3d890be79bed4433a339a61ed736d2b1a02aeb
livilammister <livilammister@gmail.com>
2018-07-02 16:37:20 -0500

    dummy tasks page

--- client/app.js
+++ client/app.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import { Navigation } from './components'
+import { TasksPage } from './components/tasks'
 
 const App = () => {
   return (
     <div>
       <Navigation />
+      <Route exact path='/tasks' component={TasksPage}/>
     </div>
   )
 }
--- client/components/navigation.js
+++ client/components/navigation.js
@@ -5,9 +5,10 @@ import { Menu } from 'semantic-ui-react'
 const Navigation = () => {
   return (
     <Menu>
-      <Menu.Item as={Link} to='/'      name='Dashboard'/>
-      <Menu.Item as={Link} to='/tasks' name='Tasks'/>
-      <Menu.Item as={Link} to='/stats' name='Stats'/>
+      <Menu.Item as={Link} to='/'        name='Dashboard' />
+      <Menu.Item as={Link} to='/tasks'   name='Tasks'     />
+      <Menu.Item as={Link} to='calendar' name='Calendar'  />
+      <Menu.Item as={Link} to='/stats'   name='Stats'     />
     </Menu>
   )
 }
