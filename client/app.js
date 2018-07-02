d1eb14ea590352ebcb55cd54d20140f780e611d9
livilammister <livilammister@gmail.com>
2018-07-02 16:52:26 -0500

    dummy calendar

--- client/app.js
+++ client/app.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-import { Navigation } from './components'
-import { StatsPage } from './components/stats'
-import { TasksPage } from './components/tasks'
+import { Navigation }   from './components'
+import { CalendarPage } from './components/calendar'
+import { StatsPage }    from './components/stats'
+import { TasksPage }    from './components/tasks'
 
 const App = () => {
   return (
     <div>
       <Navigation />
-      <Route exact path='/tasks' component={TasksPage}/>
-      <Route exact path='/stats' component={StatsPage}/>
+      <Route exact path='/tasks'    component={TasksPage}    />
+      <Route exact path='/calendar' component={CalendarPage} />
+      <Route exact path='/stats'    component={StatsPage}    />
     </div>
   )
 }
