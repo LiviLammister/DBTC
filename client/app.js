04ca841d5ec3700d1677ff834c8a2ffa238afc39
livilammister <livilammister@gmail.com>
2018-07-02 16:57:00 -0500

    dummy dashboard page

--- client/app.js
+++ client/app.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
-import { Navigation }   from './components'
-import { CalendarPage } from './components/calendar'
-import { StatsPage }    from './components/stats'
-import { TasksPage }    from './components/tasks'
+import { CalendarPage }  from './components/calendar'
+import { DashboardPage } from './components/dashboard'
+import { Navigation }    from './components'
+import { StatsPage }     from './components/stats'
+import { TasksPage }     from './components/tasks'
 
 const App = () => {
   return (
     <div>
       <Navigation />
-      <Route exact path='/tasks'    component={TasksPage}    />
-      <Route exact path='/calendar' component={CalendarPage} />
-      <Route exact path='/stats'    component={StatsPage}    />
+      <Route exact path='/'         component={DashboardPage} />
+      <Route exact path='/tasks'    component={TasksPage}     />
+      <Route exact path='/calendar' component={CalendarPage}  />
+      <Route exact path='/stats'    component={StatsPage}     />
     </div>
   )
 }
